942a8e66c79ea9038aa39b1906061aa14cc5f403
wadimba777 <vadim345plotnikov@gmail.com>
2024-03-23 20:25:51 +0300

    Add classes: DirectorDAO, MovieDAO

---
+++ src/main/java/com/moviesearch/dao/DirectorDAO.java
@@ -0,0 +1,76 @@
+package com.moviesearch.dao;
+
+import com.moviesearch.model.Director;
+import com.moviesearch.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectorDAO extends AbstractDAO<Director> {
+    @Override
+    public void add(Director director) {
+        String query = "INSERT INTO directors(name) VALUES (?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, director.getName());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Director> getAll() {
+        List<Director> directors = new ArrayList<>();
+        String query = "SELECT id, name FROM directors";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                directors.add(new Director(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return directors;
+}
+
+@Override
+public Director getById(int id) {
+    String query = "SELECT id, name FROM directors WHERE id = ?";
+    try (Connection connection = DatabaseConnection.getConnection();
+         PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return new Director(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name")
+            );
+        }
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+    return null;
+}
+
+@Override
+public boolean removeById(int id) {
+    String query = "DELETE FROM directors WHERE id = ?";
+    try (Connection connection = DatabaseConnection.getConnection();
+         PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setInt(1, id);
+        int rowsDeleted = statement.executeUpdate();
+        return rowsDeleted > 0;
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+}
---
+++ src/main/java/com/moviesearch/dao/MovieDAO.java
@@ -0,0 +1,80 @@
+package com.moviesearch.dao;
+
+import com.moviesearch.model.Movie;
+import com.moviesearch.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieDAO extends AbstractDAO<Movie>{
+
+    @Override
+    public void add(Movie movie) {
+        String query = "INSERT INTO movies(title, director_id) VALUES (?, ?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, movie.getTitle());
+            statement.setInt(2, movie.getDirectorId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        List<Movie> movies = new ArrayList<>();
+        String query = "SELECT id, name FROM movies";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                movies.add(new Movie(
+                        resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getInt("director_id")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return movies;
+    }
+
+    @Override
+    public Movie getById(int id) {
+        String query = "SELECT id, name FROM movies WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return new Movie(
+                        resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getInt("director_id")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean removeById(int id) {
+        String query = "DELETE FROM movies WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            int rowsDeleted = statement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
