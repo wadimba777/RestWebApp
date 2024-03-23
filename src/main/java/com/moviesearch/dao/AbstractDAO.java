136d92d1593d3b5f88ba87cae11ddf9011f3feeb
wadimba777 <vadim345plotnikov@gmail.com>
2024-03-23 16:36:11 +0300

    Add abstract dao class

---
+++ src/main/java/com/moviesearch/dao/AbstractDAO.java
@@ -0,0 +1,10 @@
+package com.moviesearch.dao;
+
+import java.util.List;
+
+public abstract class AbstractDAO <T> {
+    public abstract void add(T t);
+    public abstract List<T> getAll();
+    public abstract T getById(int id);
+    public abstract void remove(T t);
+}
